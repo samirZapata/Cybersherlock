5d69c58134bddf807f3f40fa23c00e63bdb5ffe5
Yonier Zapata Asprilla <samirzapata11@gmail.com>
2023-12-03 21:39:31 -0500

    Login creado exitosamente

--- app/src/main/java/com/usbbog/cbs_app/networking/Network.java
+++ app/src/main/java/com/usbbog/cbs_app/networking/Network.java
@@ -5,7 +5,7 @@ public class Network {
     /*YONIER'S IP C: 192.168.3.158
     * YONIER'S IP H: 192.168.0.8*/
     final String apiSingUp = "http://192.168.20.27:9000/api/auth/singup";
-    final String apiSinIn = "192.168.0.8:9000/api/auth/singin";
+    final String apiSinIn = "http://192.168.20.27:9000/api/auth/singin";
 
     public String getApiSingUp() {
         return apiSingUp;
--- app/src/main/java/com/usbbog/cbs_app/view/Login.java
+++ app/src/main/java/com/usbbog/cbs_app/view/Login.java
@@ -3,33 +3,138 @@ package com.usbbog.cbs_app.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.usbbog.cbs_app.R;
+import com.usbbog.cbs_app.networking.Network;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class Login extends AppCompatActivity {
 
     Button btnLogin, btnSingUp;
+    EditText txtCorreo, txtPass;
+
+    private Network url = new Network();
+    private String apiUrl = url.getApiSinIn();
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        requestQueue = Volley.newRequestQueue(this);
 
         //HOOKS
         btnLogin = findViewById(R.id.btnLogin);
         btnSingUp = findViewById(R.id.btnNUser);
+        txtCorreo = findViewById(R.id.edtUserlg);
+        txtPass = findViewById(R.id.edtPasslg);
 
-       btnSingUp.setOnClickListener((View view)->{
-           Intent i = new Intent(Login.this, Sing_up.class);
-           startActivity(i);
-       });
+        btnSingUp.setOnClickListener((View view) -> {
+            Intent i = new Intent(Login.this, Sing_up.class);
+            startActivity(i);
+        });
 
-
+        btnLogin.setOnClickListener((View view) -> {
+            sing_In(apiUrl);
+        });
 
     }
+
+
+    private void sing_In(String baseUrl) {
+        Log.i("URL ROUTE: ", baseUrl);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, baseUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+
+
+                    //JSONObject object = new JSONObject();
+
+                    //VERIFICACION DEL TOKEN
+                    //String correo = object.getString("correo");
+                    //String pass = object.getString("password");
+                    //if (correo.equals(txtCorreo.getText().toString()) && pass.equals(txtPass.getText().toString())) {
+                        Toast.makeText(Login.this, "¡Oh!, Has regresado", Toast.LENGTH_SHORT).show();
+                        Intent j = new Intent(Login.this, Sing_up.class);
+                        startActivity(j);
+                    //} else {
+
+                   // }
+                } catch (Exception e) {
+                    Toast.makeText(Login.this, "¡Credenciales incorrectas!", Toast.LENGTH_SHORT).show();
+                    Log.e("ERROR: ", e + "");
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error: ", error + "");
+
+                //VERIFICA SI EL ERROR ES UN 401
+                if (error instanceof AuthFailureError) {
+                    Toast.makeText(Login.this, "¡Credenciales incorrectas!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Login.this, "¡Credenciales incorrectas!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }) {
+
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            public byte[] getBody() throws AuthFailureError {
+                try {
+
+                    JSONObject jsonBody = new JSONObject();
+                    jsonBody.put("correo", txtCorreo.getText().toString());
+                    jsonBody.put("password", txtPass.getText().toString());
+
+                    // CONVIERTE EL OBJETO JSON A BYTES
+                    return jsonBody.toString().getBytes("utf-8");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return null;
+                } catch (UnsupportedEncodingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        RequestQueue RU = Volley.newRequestQueue(this);
+        RU.add(stringRequest);
+    }
+
+    private void saveTokenSharedPreferences(String token) {
+        // Guarda el token en las preferencias compartidas para su uso posterior
+        //SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        //SharedPreferences.Editor editor = preferences.edit();
+        //editor.putString("token", token);
+        //editor.apply();
+    }
+
 }
